9653484e027770bbb9541ccc44373ec552ee2aee
MICHELLEWAIRIMU04 <michelle@michelle>
2024-12-19 19:40:00 +0300

    first commit

---
+++ test.py
@@ -0,0 +1,11 @@
+print("Hello World!")
+print("Hello sun!")
+print("Hello sky!")
+
+print("Hello world!", end=" ")
+print("Hello sun!", end="!! ")
+print("Hello sky!", end="!!!\n")
+
+print("Hello Python Shell")
+first_number = 7
+print(first_number)
