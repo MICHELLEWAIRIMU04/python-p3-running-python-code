ff9811d4dc4a10ab919476b5bed658c88992b85c
MICHELLEWAIRIMU04 <michelle@michelle>
2025-01-06 13:23:45 +0300

    first commit

--- app.py
+++ app.py
@@ -1,11 +1,1 @@
-print("Hello World!")
-print("Hello sun!")
-print("Hello sky!")
-
-print("Hello world!", end=" ")
-print("Hello sun!", end="!! ")
-print("Hello sky!", end="!!!\n")
-
-print("Hello Python Shell")
-first_number = 7
-print(first_number)
+print("Hello World! Pass this test,please.")
